eb1568f0bcc915888a6317209f949e1a0bbd6531
Adam Wilson <adam@aye-creative.co.uk>
2023-02-02 13:24:54 +0000

    Rerelease with appsero included

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: ACF Block Generator
  * Description: Quickly create ACF gutenberg blocks. Settings > ACF Block Generator.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Adam Wilson
  * Author URI:  https://adamwilson.co.uk
  */
