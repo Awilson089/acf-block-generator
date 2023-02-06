0229f054093c655a3d17ee24f78750718da88852
Adam Wilson <adam@aye-creative.co.uk>
2023-02-06 12:05:20 +0000

    Added option to create acf field group

--- index.php
+++ index.php
@@ -35,135 +35,161 @@ appsero_init_tracker_acf_block_generator();
 add_action( 'admin_menu', 'create_block_add_admin_menu' );
 add_action( 'admin_init', 'create_block_settings_init' );
  
- 
 function create_block_add_admin_menu() { 
-
     add_options_page( 'ACF Block Generator', 'ACF Block Generator', 'manage_options', 'create_block', 'create_block_options_page' );
-
 }
 
-
 function create_block_settings_init(  ) { 
+    register_setting( 'pluginPage', 'create_block_settings' );
 
-register_setting( 'pluginPage', 'create_block_settings' );
+    add_settings_section(
+            'create_block_pluginPage_section', 
+            __( '', 'create_block' ), 
+            'create_block_settings_section_callback', 
+            'pluginPage'
+    );
 
-add_settings_section(
-        'create_block_pluginPage_section', 
-        __( '', 'create_block' ), 
-        'create_block_settings_section_callback', 
-        'pluginPage'
-);
+    add_settings_field( 
+            'block_name', 
+            __( 'Block Name*', 'create_block' ), 
+            'block_name_render', 
+            'pluginPage', 
+            'create_block_pluginPage_section' 
+    );
 
-add_settings_field( 
-        'block_name', 
-        __( 'Block Name*', 'create_block' ), 
-        'block_name_render', 
+    add_settings_field( 
+        'block_icon', 
+        __( 'Block Icon', 'create_block' ), 
+        'block_icon_render', 
         'pluginPage', 
         'create_block_pluginPage_section' 
-);
+    );
 
-add_settings_field( 
-    'block_icon', 
-    __( 'Block Icon', 'create_block' ), 
-    'block_icon_render', 
-    'pluginPage', 
-    'create_block_pluginPage_section' 
-);
+    add_settings_field( 
+        'block_keywords', 
+        __( 'Block Keywords', 'create_block' ), 
+        'block_keywords_render', 
+        'pluginPage', 
+        'create_block_pluginPage_section' 
+    );
 
-add_settings_field( 
-    'block_keywords', 
-    __( 'Block Keywords', 'create_block' ), 
-    'block_keywords_render', 
-    'pluginPage', 
-    'create_block_pluginPage_section' 
-);
+    add_settings_field( 
+        'block_category', 
+        __( 'Block Category', 'create_block' ), 
+        'block_category_render', 
+        'pluginPage', 
+        'create_block_pluginPage_section' 
+    );
 
-add_settings_field( 
-    'block_category', 
-    __( 'Block Category', 'create_block' ), 
-    'block_category_render', 
-    'pluginPage', 
-    'create_block_pluginPage_section' 
-);
-
+    add_settings_field( 
+        'block_create_acf', 
+        __( 'Create ACF field group?', 'create_block' ), 
+        'block_create_acf_render', 
+        'pluginPage', 
+        'create_block_pluginPage_section' 
+    );
 }
 
 
 function block_name_render(  ) { 
+    $options = get_option( 'create_block_settings' ); ?>
 
-    $options = get_option( 'create_block_settings' );
-    ?>
     <input type='text' placeholder='Example Block...' name='create_block_settings[block_name]'>
     <?php
-
 }
 
 function block_icon_render(  ) { ?>
-<input type='text' placeholder='editor-contract...' name='create_block_settings[block_icon]'>
-<p>From <a href="https://developer.wordpress.org/resource/dashicons/#podio" target="_blank">Dashicons</a>.</p>
+    <input type='text' placeholder='editor-contract...' name='create_block_settings[block_icon]'>
+    <p>From <a href="https://developer.wordpress.org/resource/dashicons/#podio" target="_blank">Dashicons</a>.</p>
 <?php
 }
 
 function block_category_render(  ) { ?>
-<select name='create_block_settings[block_category]'>
-    <option value="common">Common</option>
-    <option value="formatting">Formatting</option>
-    <option value="layout">Layout</option>
-    <option value="widgets">Widgets</option>
-    <option value="embed">Embed</option>
-</select>
+    <select name='create_block_settings[block_category]'>
+        <option value="common">Common</option>
+        <option value="formatting">Formatting</option>
+        <option value="layout">Layout</option>
+        <option value="widgets">Widgets</option>
+        <option value="embed">Embed</option>
+    </select>
+<?php
+}
+
+function block_create_acf_render(  ) { ?>
+    <input type="checkbox" id="block_create_acf" name="create_block_settings[block_create_acf]" value="0"/>
 <?php
 }
 
 function block_keywords_render(  ) { ?>
-<input type='text' placeholder='Keyword 1, Keyword 2' name='create_block_settings[block_keywords]'>
-<p>Comma seperated.</p>
+    <input type='text' placeholder='Keyword 1, Keyword 2' name='create_block_settings[block_keywords]'>
+    <p>Comma seperated.</p>
 <?php
 }
 
 
 function create_block_settings_section_callback(  ) { 
-echo __( '<p>Quickly generate an ACF block. Enter your block name like "Example Block".</p>', 'create_block' );
+    echo __( '<p>Quickly generate an ACF block. Enter your block name like "Example Block".</p>', 'create_block' );
+}
+
+function create_acf_field_group( $form_name ) {
+    $new_post = array(
+        'post_title'     => $form_name,
+        'post_excerpt'   => sanitize_title( $form_name ),
+        'post_name'      => 'group_' . uniqid(),
+        'post_date'      => date( 'Y-m-d H:i:s' ),
+        'comment_status' => 'closed',
+        'post_status'    => 'publish',
+        'post_type'      => 'acf-field-group',
+    );
+    $post_id  = wp_insert_post( $new_post );
+
+    return $post_id;
 }
 
 add_action( 'wp_ajax_create_block', 'create_block' );
 add_action( 'wp_ajax_nopriv_create_block', 'create_block' );
 
 function create_block() { 
-$name = $_POST['name'];
-$category = $_POST['category'];
-$icon = $_POST['icon'];
-$keywords = $_POST['keywords'];
-$keys = explode (', ', $keywords);   
-$keys = array_map(function($x){ return '"'.$x.'"'; }, $keys); 
+    $name = $_POST['name'];
+    $category = $_POST['category'];
+    $icon = $_POST['icon'];
+    $keywords = $_POST['keywords'];
+    $create_acf = $_POST['create_acf'];
+    $keys = explode (', ', $keywords);   
+    $keys = array_map(function($x){ return '"'.$x.'"'; }, $keys); 
 
-$slug = sanitize_title($_POST['name']);
-$file_name = '/'.$slug;
+    $slug = sanitize_title($_POST['name']);
+    $file_name = '/'.$slug;
 
-$directory = get_template_directory() .'/blocks'.$file_name;
-wp_mkdir_p($directory);
+    $directory = get_template_directory() .'/blocks'.$file_name;
+    wp_mkdir_p($directory);
 
-$php_file = fopen($directory.$file_name.'.php',"w");
-$sass_file = fopen($directory.$file_name.'.scss',"w");
-$block_file = fopen($directory.'/block.json',"w");
+    $php_file = fopen($directory.$file_name.'.php',"w");
+    $sass_file = fopen($directory.$file_name.'.scss',"w");
+    $block_file = fopen($directory.'/block.json',"w");
 
-$content = '{
-    "name": "acf/'.$slug.'",
-    "title": "'.$name.'",
-    "description": "A simple '.strtolower($name).'",
-    "category": "'.$category.'",
-    "icon": "'.$icon.'",
-    "keywords": ['.implode(', ', $keys).'],
-    "acf": {
-        "mode": "preview",
-        "renderTemplate": "'.$slug.'.php"
-    },
-    "align": "full"
-}';
-fwrite($block_file,$content);
-fclose($block_file);
+    $content = '{
+        "name": "acf/'.$slug.'",
+        "title": "'.$name.'",
+        "description": "A simple '.strtolower($name).'",
+        "category": "'.$category.'",
+        "icon": "'.$icon.'",
+        "keywords": ['.implode(', ', $keys).'],
+        "acf": {
+            "mode": "preview",
+            "renderTemplate": "'.$slug.'.php"
+        },
+        "align": "full"
+    }';
 
-wp_die();
+    fwrite($block_file,$content);
+    fclose($block_file);
+
+    if($create_acf === 'true') {
+        create_acf_field_group($name);
+    }
+
+    wp_die();
 }
 
 
@@ -176,6 +202,7 @@ function create_block_options_page(  ) {  ?>
                 var category = $('select[name="create_block_settings[block_category]"]').val();
                 var icon = $('input[name="create_block_settings[block_icon]"]').val();
                 var keywords = $('input[name="create_block_settings[block_keywords]"]').val();
+                var create_acf = $('input[name="create_block_settings[block_create_acf]"]').is(':checked');
 
                 if(name != '') {
                     $.ajax({
@@ -188,10 +215,13 @@ function create_block_options_page(  ) {  ?>
                             category: category,
                             icon: icon,
                             keywords: keywords,
+                            create_acf: create_acf,
                         },
                         success: function (data) {
-                            console.log(data);
-                            $('.message').html(name+' block created.');
+                            $('.message').html('<p>'+name+' block created.</p>');
+                            if(create_acf == true) {
+                                $('.message').append('<p>'+name+' field group created.</p>');
+                            }
                         },
                         error: function(MLHttpRequest, textStatus, errorThrown){
                             console.log(errorThrown);
@@ -204,7 +234,7 @@ function create_block_options_page(  ) {  ?>
         });
     </script>
 
-        <form action="<?php echo admin_url( 'admin-post.php' ); ?>">
+    <form action="<?php echo admin_url( 'admin-post.php' ); ?>">
         <input type="hidden" name="action" value="create_block">
 
             <h1>ACF Block Generator</h1>
@@ -215,9 +245,9 @@ function create_block_options_page(  ) {  ?>
             submit_button( __( 'Create Block', 'textdomain' ), 'primary', 'submit-form', true );
             ?>
 
-        <div class="message"></div>
-        </form>
-        <?php
+            <div class="message"></div>
+    </form>
+    <?php
 
 }
  
